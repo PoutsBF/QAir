ba3381c8793983f3f5e293789d86afb65679b903
PoutsBF <stephane.lepoutere@free.fr>
2022-12-09 22:52:16 +0100

    installation websocket

--- lib/webServeur/src/webServeur.cpp
+++ lib/webServeur/src/webServeur.cpp
@@ -31,6 +31,8 @@ WebServeur::~WebServeur()
 uint8_t WebServeur::init()
 {
     server = new AsyncWebServer(80);
+    ws = new AsyncWebSocket("/ws");
+    events = new AsyncEventSource("/events");
 
     // Initialize SPIFFS
     if (!SPIFFS.begin(true))
@@ -67,8 +69,13 @@ uint8_t WebServeur::init()
     Serial.println(WiFi.localIP());
 #endif
 
-    // server->on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-    //           { request->send(200, "text/plain", "Hello, world"); });
+    // Configuration des évènements web server
+    ws->onEvent(onWsEvent);
+    server->addHandler(ws);
+
+    events->onConnect([](AsyncEventSourceClient *client)
+                     { client->send("hello!", NULL, millis(), 1000); });
+    server->addHandler(events);
 
     server->on("/heap", HTTP_GET, [](AsyncWebServerRequest *request)
               {
@@ -79,6 +86,7 @@ uint8_t WebServeur::init()
                 //  ws.count(),
                  WiFi.RSSI());
         request->send(200, "text/plain", String(texte)); });
+
     server->on("/reset", HTTP_GET, [](AsyncWebServerRequest *request)
               {
         request->send(200, "text/plain", String("Reset en cours..."));
@@ -96,4 +104,95 @@ uint8_t WebServeur::init()
 void WebServeur::notFound(AsyncWebServerRequest *request)
 {
     request->send(404, "text/plain", "Not found");
+}
+
+void WebServeur::onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+//    extern CValeurs valeurs;
+
+    if (type == WS_EVT_CONNECT)
+    {
+        Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+//        valeurs.envoie(client->id());
+//        valeurs.envoie(client->id());
+    }
+    else if (type == WS_EVT_DISCONNECT)
+    {
+        Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+    }
+    else if (type == WS_EVT_ERROR)
+    {
+        Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+    }
+    else if (type == WS_EVT_PONG)
+    {
+        Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+    }
+    else if (type == WS_EVT_DATA)
+    {
+        AwsFrameInfo *info = (AwsFrameInfo *)arg;
+        String msg = "";
+        if (info->final && info->index == 0 && info->len == len)
+        {
+            // the whole message is in a single frame and we got all of it's data
+            Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+
+            if (info->opcode == WS_TEXT)
+            {
+                for (size_t i = 0; i < info->len; i++)
+                {
+                    msg += (char)data[i];
+                }
+            }
+            else
+            {
+                char buff[3];
+                for (size_t i = 0; i < info->len; i++)
+                {
+                    sprintf(buff, "%02x ", (uint8_t)data[i]);
+                    msg += buff;
+                }
+            }
+            Serial.printf("%s\n", msg.c_str());
+        }
+        else
+        {
+            // message is comprised of multiple frames or the frame is split into multiple packets
+            if (info->index == 0)
+            {
+                if (info->num == 0)
+                    Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+                Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+            }
+
+            Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
+
+            if (info->opcode == WS_TEXT)
+            {
+                for (size_t i = 0; i < len; i++)
+                {
+                    msg += (char)data[i];
+                }
+            }
+            else
+            {
+                char buff[3];
+                for (size_t i = 0; i < len; i++)
+                {
+                    sprintf(buff, "%02x ", (uint8_t)data[i]);
+                    msg += buff;
+                }
+            }
+            Serial.printf("%s\n", msg.c_str());
+
+            if ((info->index + len) == info->len)
+            {
+                Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+                if (info->final)
+                {
+                    Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+                }
+            }
+        }
+    }
 }
--- lib/webServeur/src/webServeur.h
+++ lib/webServeur/src/webServeur.h
@@ -14,17 +14,22 @@ Librairie pour la gestion du serveur web
 #endif
 #include <ESPAsyncWebServer.h>
 
+//#include <json_parser.h>
+
 class WebServeur
 {
 private:
     AsyncWebServer *server;
+    AsyncWebSocket *ws;
+    AsyncEventSource *events;
 
-    /* data */
+    static void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+    static void notFound(AsyncWebServerRequest *request);
+
 public:
     WebServeur(/* args */);
     ~WebServeur();
 
     uint8_t init(void);
-    static void notFound(AsyncWebServerRequest *request);
 };
 
